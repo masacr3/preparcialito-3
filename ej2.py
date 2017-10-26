0c57bd50208c9b80f8ebd5895d5e05f397500336
Leonel R <leorhcp2030@gmail.com>
2017-10-26 00:14:31 -0300

    Update ej2.py

--- ej2.py
+++ ej2.py
@@ -1,1 +1,24 @@
+#pila TDA con todas sus primitivas
+
+def invertir_lista(self):
+	if not self.primero or not self.primero.sig :
+		return None
+	
+	primero = self.primero
+	recorrer = self.primero
+	pila = PILA()
+	
+	while recorrer:
+		pila.apila(recorrer)
+		recorrer = recorrer.sig
+	
+	primero = pila.desapila()
+	
+	while not pila.esta_vacia():
+		primero.sig = pila.desapila()
+		primero = primero.sig
+	
+	primero.sig = None
+	
+
 
