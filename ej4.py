37e0ee3137e65dabc1c3b8ec89d99de8ecdef571
Leonel R <leorhcp2030@gmail.com>
2017-10-26 01:16:02 -0300

    Update ej4.py

--- ej4.py
+++ ej4.py
@@ -1,1 +1,23 @@
+def suma_acumulativa(self):
+	if not self.primero:
+		return None
+	
+	lista_nueva = ListaEnlazada(self.primero.value)
+	
+	if not self.primero.sig:
+		return lista_nueva
+	
+	acumulador = self.primero.value
+	actual = self.primero.sig
+	
+	while actual:
+		acumulador += actual.value
+		lista_nueva.push(acumulador)
+	
+	return lista_nueva
+	
+	
+	
+	
 
+		
