4fced0ee8c8c19db6bb5a647415073cb4c3cb293
Leonel R <leorhcp2030@gmail.com>
2017-10-26 00:31:58 -0300

    Update ej1.py

--- ej1.py
+++ ej1.py
@@ -1,1 +1,14 @@
-
+def reemplazar(pila,nuevo,viejo):
+	
+	if pila.esta_vacia():
+		return None
+	
+	_pila = PILA()
+	
+	while not pila.esta_vacia():
+		value = pila.desapila()
+		_pila.apila( nuevo if value == viejo else value )
+	
+	while not _pila.esta_vacia():
+		pila.apila(_pila.desapila())
+	
