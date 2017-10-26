8add91cb90ff39fe33d4e79bde7942fe7135797c
Leonel R <leorhcp2030@gmail.com>
2017-10-26 01:38:05 -0300

    Update ej6.py

--- ej6.py
+++ ej6.py
@@ -1,2 +1,16 @@
-es_palindromo(cola,n_elementos):
+def es_palindromo(cola,n_elementos):
+	""" pre: se asume que la cola no esta vacia y n_elementos es el 
+		numero de elementos 
+	"""     
+	pila = PILA()
+	for i in range(n_elementos // 2):
+		pila.apila(cola.desencola())
 	
+	for i in range(n_elementos // 2):
+		Letra,letra2 = cola.desencolar(),pila.desapilar()
+		if letra != letra2:
+			return False
+	
+	return True
+	
+		
