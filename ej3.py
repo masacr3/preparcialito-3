486be50fca2cfb4a108191240337a7edbdb28d26
Leonel R <leorhcp2030@gmail.com>
2017-10-26 00:58:54 -0300

    Update ej3.py

--- ej3.py
+++ ej3.py
@@ -1,1 +1,15 @@
-
+def remover_duplicados_consecutivos(pila):
+	if pila.esta_vacia():
+		return None
+	
+	lista = [pila.desapila]
+	
+	while not pila.esta_vacia():
+		value = pila.desapila()
+		if value not in lista:
+			fila.append(value)
+		
+	for i in range(len(lista)):
+		pila.apila(lista.pop())
+			
+		
